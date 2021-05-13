a8fa15420dbc436916658f28a51e07eb3194db65
Valentin Tamone <tamtam.valentin@gmail.com>
2021-05-13 13:16:56 +0200

    request bpm

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3001
-const EmpaticaE4 = require('empatica-e4-client');
-const dev1 = new EmpaticaE4();
-const portNumber = 28000;
-const ipAddress = '127.0.0.1';
-const deviceID = '484c5c';        //Empatica E4 device ID //543a64 ou 484c5c
+const EmpaticaE4 = require('empatica-e4-client')
+const dev1 = new EmpaticaE4()
+const portNumber = 28000
+const ipAddress = '127.0.0.1'
+const deviceID = '484c5c'        //Empatica E4 device ID //543a64 ou 484c5c
+
+let count = 0;
+let addedBPM = 0;
+let lastAverageBPM = 0;
 
 try {
     dev1.connect(portNumber, ipAddress, deviceID, (data) => {
@@ -16,21 +21,35 @@ try {
     console.log("error")
 }
 
-app.get("/bpm",(req,res)=>{
-    //TODO send bpm to front, idealement une moyenne, comme on get depuis le front que toutes les 2sec
+app.get("/bpm", (req,res)=>{
+    if (count == 0){
+        res.send(lastAverageBPM)
+    }
+    else{
+        let averageBPM = addedBPM/count
+        lastAverageBPM = averageBPM
+        count = 0
+        addedBPM = 0
+        res.send(averageBPM)
+    }
 })
 
 setTimeout(()=>{
-    const cmd = 'device_subscribe ' + EmpaticaE4.E4_IBI + ' ON \n';
-    const cmdBinary = EmpaticaE4.conv(cmd, {in: 'binary'});
+    const cmd = 'device_subscribe ' + EmpaticaE4.E4_IBI + ' ON \n'
+    const cmdBinary = EmpaticaE4.conv(cmd, {in: 'binary'})
+
     EmpaticaE4.client.write(cmdBinary,'ascii')
+
     EmpaticaE4.client.on("data", (message) => {
-        const split = message.toString().split("\n");
+        const split = message.toString().split("\n")
         split.forEach(line => {
             if(line.includes("Hr")) {
                 let i = line.lastIndexOf(" ")
                 let hr = line.toString().substring(i, line.length)
                 console.log("Hr = " + hr)
+                count += 1
+                addedBPM += parseInt(hr, 10)
+                console.log("count = " + count + "\nadded hr = " + addedBPM)
             }
         })
     });
